ae14d706d8233f9edc283909fa80b74449219c40
acl <acl447@nyu.edu>
2019-03-17 17:09:44 -0700

    edits march 17

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,9 @@ $(document).ready(function () {
 
     var unansweredQuestionsCounter = 8;
 
+    var counter = 90;
+
+
 
 
 
@@ -32,12 +35,8 @@ $(document).ready(function () {
 
 
 
-        var counter = 90;
-
         var countDown = setInterval(function () {
             counter--;
-
-            console.log(counter);
 
             if (counter === 0) {
 
@@ -47,11 +46,8 @@ $(document).ready(function () {
 
                 alert("Correct Answers: " + correctAnswersCounter + " " +
                     "Incorrect Answers: " + incorrectAnswersCounter + " " +
-                    "Unanswered Questions: " + unansweredQuestionsCounter);
-
-
-
-
+                    "Unanswered Questions: " + unansweredQuestionsCounter
+                );
 
             }
 
@@ -63,263 +59,167 @@ $(document).ready(function () {
 
 
 
+
     });
 
-    $("input[name='answers-1']").click(function () {
 
-        //Gets the value of whichever button is clicked in each radio button group
+
+
+
+    $("#submit").click(function () {
+
+        console.log("button clicked");
+
+        //Gets the value of whichever button is clicked in each group of radio buttons
 
         var radioValue = $("input[name='answers-1']:checked").val();
 
-        
+        var radioValue2 = $("input[name='answers-2']:checked").val();
 
+        var radioValue3 = $("input[name='answers-3']:checked").val();
+
+        var radioValue4 = $("input[name='answers-4']:checked").val();
+
+        var radioValue5 = $("input[name='answers-5']:checked").val();
+
+        var radioValue6 = $("input[name='answers-6']:checked").val();
+
+        var radioValue7 = $("input[name='answers-7']:checked").val();
+
+        var radioValue8 = $("input[name='answers-8']:checked").val();
 
         if (radioValue) {
 
-            console.log(radioValue);
-
             if (radioValue == 1) {
 
-                console.log("Correct");
-
                 correctAnswersCounter++;
-
-                console.log(correctAnswersCounter);
             }
 
             else if (radioValue == 0) {
 
-                console.log("Incorrect");
-
                 incorrectAnswersCounter++;
-
-                console.log(incorrectAnswersCounter);
-
-
             }
-        }
 
-    });
-
-    $("input[name='answers-2']").click(function () {
-
-        var radioValue2 = $("input[name='answers-2']:checked").val();
+        };
 
         if (radioValue2) {
 
-            console.log(radioValue2);
-
             if (radioValue2 == 1) {
-
-                console.log("Correct");
 
                 correctAnswersCounter++;
 
-                console.log(correctAnswersCounter);
             }
 
             else if (radioValue2 == 0) {
 
-                console.log("Incorrect");
-
                 incorrectAnswersCounter++;
 
-                console.log(incorrectAnswersCounter);
-
-
             }
-        }
-
-    });
-
-    $("input[name='answers-3']").click(function () {
-
-        var radioValue3 = $("input[name='answers-3']:checked").val();
-
-
+        };
 
         if (radioValue3) {
 
-            console.log(radioValue3);
-
             if (radioValue3 == 1) {
-
-                console.log("Correct");
 
                 correctAnswersCounter++;
 
-                console.log(correctAnswersCounter);
             }
 
             else if (radioValue3 == 0) {
 
-                console.log("Incorrect");
-
                 incorrectAnswersCounter++;
 
-                console.log(incorrectAnswersCounter);
-
-
             }
-        }
-
-    });
-
-    $("input[name='answers-4']").click(function () {
-
-        var radioValue4 = $("input[name='answers-4']:checked").val();
+        };
 
         if (radioValue4) {
 
-            console.log(radioValue4);
-
             if (radioValue4 == 1) {
 
-                console.log("Correct");
-
                 correctAnswersCounter++;
-
-                console.log(correctAnswersCounter);
             }
 
             else if (radioValue4 == 0) {
 
-                console.log("Incorrect");
-
                 incorrectAnswersCounter++;
 
-                console.log(incorrectAnswersCounter);
-
-
             }
-        }
-
-    });
-
-    $("input[name='answers-5']").click(function () {
-
-
-        var radioValue5 = $("input[name='answers-5']:checked").val();
+        };
 
         if (radioValue5) {
 
-            console.log(radioValue5);
-
             if (radioValue5 == 1) {
-
-                console.log("Correct");
 
                 correctAnswersCounter++;
 
-                console.log(correctAnswersCounter);
             }
 
             else if (radioValue5 == 0) {
 
-                console.log("Incorrect");
-
                 incorrectAnswersCounter++;
 
-                console.log(incorrectAnswersCounter);
-
-
             }
-        }
 
-    });
 
-    $("input[name='answers-6']").click(function () {
-
-        var radioValue6 = $("input[name='answers-6']:checked").val();
-
+        };
 
         if (radioValue6) {
 
-            console.log(radioValue6);
-
             if (radioValue6 == 1) {
-
-                console.log("Correct");
 
                 correctAnswersCounter++;
 
-                console.log(correctAnswersCounter);
             }
 
             else if (radioValue6 == 0) {
 
-                console.log("Incorrect");
-
                 incorrectAnswersCounter++;
 
-                console.log(incorrectAnswersCounter);
-
-
             }
-        }
 
-    });
-
-    $("input[name='answers-7']").click(function () {
-
-        var radioValue7 = $("input[name='answers-7']:checked").val();
+        };
 
         if (radioValue7) {
 
-            console.log(radioValue7);
-
             if (radioValue7 == 1) {
-
-                console.log("Correct");
 
                 correctAnswersCounter++;
 
-                console.log(correctAnswersCounter);
             }
 
             else if (radioValue7 == 0) {
 
-                console.log("Incorrect");
-
                 incorrectAnswersCounter++;
 
-                console.log(incorrectAnswersCounter);
-
-
             }
-        }
 
-    });
-
-
-    $("input[name='answers-8']").click(function () {
-
-        var radioValue8 = $("input[name='answers-8']:checked").val();
+        };
 
         if (radioValue8) {
 
-            console.log(radioValue8);
-
             if (radioValue8 == 1) {
-
-                console.log("Correct");
 
                 correctAnswersCounter++;
 
-                console.log(correctAnswersCounter);
             }
 
             else if (radioValue8 == 0) {
 
-                console.log("Incorrect");
-
                 incorrectAnswersCounter++;
 
-                console.log(incorrectAnswersCounter);
-
-
             }
-        }
+
+        };
+
+
+
+        unansweredQuestionsCounter -= (correctAnswersCounter + incorrectAnswersCounter);
+
+        alert("Correct Answers: " + correctAnswersCounter + " " +
+            "Incorrect Answers: " + incorrectAnswersCounter + " " +
+            "Unanswered Questions: " + unansweredQuestionsCounter
+        );
+
 
     });
 
